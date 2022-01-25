e9669e39ea7ec95d8c728d3bf0323aa05e19a1cf
GitHub Action <action@github.com>
2022-01-25 21:37:45 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/phutureproof/modelcreatorplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.phutureproof.modelcreatorplugin.services
+
+import com.github.phutureproof.modelcreatorplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/phutureproof/modelcreatorplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/phutureproof/modelcreatorplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.phutureproof.modelcreatorplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.phutureproof.modelcreatorplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
